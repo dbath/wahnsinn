7f4afd67bfe303c48034675bd3355e1931c19ae1
dbath <danbath@gmail.com>
2015-01-19 12:40:03 -0500

    temp

--- flymad_jaaba/tempOOP.py
+++ flymad_jaaba/tempOOP.py
@@ -1,22 +1,22 @@
 
-import utilities.utilities as utilities
+import flymad_jaaba.utilities as utilities
 import flymad_jaaba.target_detector as target_detector
 
 import matplotlib.pyplot as plt
 
 
-fmf_file = '/media/DBATH_6/150108/DB201-GP-costim_wide_20150108_185354.fmf'
+fmf_file = '/media/DBATH_5/150107/DB201-GP-costim_wide_20150107_185416.fmf'
 
-bagdir = '/media/DBATH_6/150108/BAGS'
+bagdir = '/media/DBATH_5/150107/BAGS'
 
 
-tempdir = '/groups/dickson/home/bathd/Desktop/'
+tempdir = '/groups/dickson/home/bathd/Desktop/150119'
 
 
 bag_file = utilities.match_fmf_and_bag(fmf_file, bagdir)
 
 targs = target_detector.TargetDetector(fmf_file, tempdir)
-
+targs.plot_trajectory_on_background(bag_file)
 
 positions = utilities.get_positions_from_bag(bag_file)
 distances = targs.get_dist_to_nearest_target(bag_file)
@@ -25,5 +25,5 @@ distances = targs.get_dist_to_nearest_target(bag_file)
 plt.plot(distances)
 plt.show()
 
-plt.plot(positions.fly_x, positions.fly_y, alpha=distances.values)
+plt.plot(positions.fly_x, positions.fly_y)#, alpha=distances.values)
 plt.show()
