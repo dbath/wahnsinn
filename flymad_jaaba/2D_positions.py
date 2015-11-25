8037c790a24bc6c9aa7344c4bcdbaf14ce5c5d73
dbath <danbath@gmail.com>
2015-11-25 11:24:43 -0500

    implemented general functionality to this plot generator. Call with parent flymad directory to make one plot per experimental group

--- flymad_jaaba/2D_positions.py
+++ flymad_jaaba/2D_positions.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import glob
+import argparse
 
 
 def parse_fmftime(namestring):
@@ -16,36 +17,54 @@ def parse_fmftime(namestring):
 
 
 
-fullData = pd.DataFrame({'minutes':[],'fly_x':[],'fly_y':[],'FlyID':[],'GROUP':[]})
+def doit(_DATADIR, _HANDLE):
 
+    fullData = pd.DataFrame({'minutes':[],'fly_x':[],'fly_y':[],'FlyID':[],'GROUP':[]})
 
-DATADIR = '/media/DBATH_10/151029_P1_adt2_coact/'
-HANDLE = 'DB139'
+    for directory in glob.glob(_DATADIR + '*' + _HANDLE + '*' + '*zoom*'):
+        FLY_ID, FMF_TIME, GROUP = parse_fmftime(directory)
+        try:
+            df = pd.read_pickle(directory + '/frame_by_frame_synced.pickle')
+            df['minutes'] = df.synced_time.astype('timedelta64[m]')
+            tempdf = pd.DataFrame({'minutes':df.minutes, 'fly_x':df.fly_x, 'fly_y':df.fly_y, 'FlyID':FLY_ID, 'group':GROUP})
+            fullData = pd.concat([fullData, tempdf], axis=0)
+        except:
+            pass
 
-for directory in glob.glob(DATADIR + '*' + HANDLE + '*' + '*zoom*'):
-    FLY_ID, FMF_TIME, GROUP = parse_fmftime(directory)
-    try:
-        df = pd.read_pickle(directory + '/frame_by_frame_synced.pickle')
-        df['minutes'] = df.synced_time.astype('timedelta64[m]')
-        tempdf = pd.DataFrame({'minutes':df.minutes, 'fly_x':df.fly_x, 'fly_y':df.fly_y, 'FlyID':FLY_ID, 'group':GROUP})
-        fullData = pd.concat([fullData, tempdf], axis=0)
-    except:
-        pass
+    fullData.to_pickle(_DATADIR + '2D_positions_' + _HANDLE + '.pickle')
 
-fullData.to_pickle(DATADIR + HANDLE + '_position_data.pickle')
+    plt.figure(figsize=(24,2))
 
-plt.figure(figsize=(24,2))
+    for x in np.arange(-1,11):
+        plt.subplot2grid((1,49),(0,4*(x+1)), colspan=4)
+        slyce = fullData[fullData.minutes == x]
+        print x, len(slyce), len(slyce.fly_x.notnull())
+        plt.hist2d(slyce.fly_x.values, slyce.fly_y.values, bins=50, norm=LogNorm())
+        plt.xlim(0,659)
+        plt.ylim(0,494)
+        plt.axis('off')
+        if x == 10:
+            axes = plt.subplot2grid((1,49),(0,48), colspan=1)
+            plt.colorbar(cax=axes)
 
-for x in np.arange(-1,11):
-    plt.subplot2grid((1,49),(0,4*(x+1)), colspan=4)
-    slyce = fullData[fullData.minutes == x]
-    print x, len(slyce), len(slyce.fly_x.notnull())
-    plt.hist2d(slyce.fly_x.values, slyce.fly_y.values, bins=50, norm=LogNorm())
-    plt.xlim(0,659)
-    plt.ylim(0,494)
-    plt.axis('off')
-    if x == 10:
-        axes = plt.subplot2grid((1,49),(0,48), colspan=1)
-        plt.colorbar(cax=axes)
+    plt.savefig(_DATADIR + 'position_plots_' + _HANDLE + '.svg', bbox_inches='tight')
+    return
 
-plt.savefig(DATADIR + 'position_plots_' + HANDLE + '.svg', bbox_inches='tight')
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--flymad_dir', type=str, required=True,
+                            help='directory of flymad files')  
+    args = parser.parse_args()
+
+    DATADIR = args.flymad_dir 
+    if (DATADIR[-1] != '/'):
+        DATADIR = DATADIR + '/'                      
+    listy =[]
+    for directory in glob.glob(DATADIR  + '*zoom*'):
+        FLY_ID, FMF_TIME, GROUP = parse_fmftime(directory)
+        listy.append(GROUP)
+    for x in list(set(listy)):
+        doit(DATADIR, x)
+    
+        
